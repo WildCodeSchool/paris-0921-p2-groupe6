37f81807a03dbd48fb79a5690ad0a399eb41b0cb
Your Name <you@example.com>
2021-11-03 16:16:43 +0100

    Adjust function and add flip card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
+import More from '../Assets/More.png';
 import './Card.css';
 
 function Card() {
   const [isFavorite, setIsFavorite] = useState(false);
-  const [more, setMore] = useState(false);
+  const [more, setMore] = useState(true);
+  const [isAdded, setIsAdded] = useState(false);
 
   function handleClickFavorite() {
     setIsFavorite(!isFavorite);
+  }
+
+  function handleClickAdded() {
+    setIsAdded(!isAdded);
   }
 
   function handleClickFlip() {
@@ -16,25 +22,30 @@ function Card() {
   return (
     <div className="Card">
       <div className="img-container">
-        <div className="front">
-          <h3 className="desc">Burger with fries</h3>
-          <img src="https://cdn.pixabay.com/photo/2017/08/12/18/59/snack-2635035_960_720.jpg" alt="Burger with fries" />
-          <button className="material-icons-outlined" id={isFavorite ? 'isFavorite' : 'notFavorite'} onClick={handleClickFavorite}>
-            star
-          </button>
-          <span className="material-icons-outlined" id="add">
-            bookmark_add
-          </span>
-          <button className="btn-more" onClick={handleClickFlip}>
-            More
-          </button>
-        </div>
-        <div className="back">
-          <p className="desc-back"> here there is the description</p>
-          <button className="btn-close" id="close">
-            Close
-          </button>
-        </div>
+        {more ? (
+          <div className="front">
+            <h3 className="desc">Burger with fries</h3>
+            <img src="https://cdn.pixabay.com/photo/2017/08/12/18/59/snack-2635035_960_720.jpg" alt="Burger with fries" />
+            <button className="material-icons-outlined" id={isFavorite ? 'isFavorite' : 'notFavorite'} onClick={handleClickFavorite}>
+              star
+            </button>
+            <button className="material-icons-outlined" id={isAdded ? 'isAdd' : 'notAdd'} onClick={handleClickAdded}>
+              bookmark_add
+            </button>
+            <button className="btn-more" onClick={handleClickFlip}>
+              <img src={More} alt="more" />
+            </button>
+          </div>
+        ) : (
+          <div className="back">
+            <p className="desc-back" id="p-back">
+              here there is the description
+            </p>
+            <button className="material-icons-outlined" id="close" onClick={handleClickFlip}>
+              cancel
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
