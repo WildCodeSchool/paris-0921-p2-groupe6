ba57a592ace2d200a72d6a6843aeef95f95cf397
Eti-Fromentin <fromentin.etienne@gmail.com>
2021-11-04 17:11:47 +0100

    fetched recipe

--- src/Components/fetchrecipe.jsx
+++ src/Components/fetchrecipe.jsx
@@ -1,49 +1,24 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import RecipeCard from './RecipeCard';
 
 function FetchRecipe() {
-  //find recipe by nutrient Cholesterol 50, Carbs 50, Calories 600, Fat 50, num 50, random
+  //find recipe by nutrient: Cholesterol 30, Carbs 30, Calories 400, Fat 30, max sugar 10, random, num 10 during testing, num 300 once in production
   const recipeByNutrient =
-    'https://api.spoonacular.com/recipes/findByNutrients?minCholesterol=50&minCarbs=50&ḿinFat=50&ḿinCalories=600&number=20&random=true&apiKey=a7c77eb73d9c496aa2a7698a71175d78';
+    'https://api.spoonacular.com/recipes/findByNutrients?minCholesterol=30&minCarbs=30&minFat=30&minCalories=400&maxSugar=10&number=10&random=true&apiKey=7706683273f24fdcaf86cbbb8929f962';
 
-  //   const [fetchedRecipeId, setFetchedRecipeId] = useState();
-  //   const [fetchedRecipeTitle, setFetchedRecipeTitle] = useState();
-  //   const [fetchedRecipeImage, setFetchedRecipeImage] = useState();
-  //   const [fetchedRecipeCalories, setFetchedRecipeCalories] = useState();
-  //   const [fetchedRecipeProtein, setFetchedRecipeProtein] = useState();
-  //   const [fetchedRecipeCarbs, setFetchedRecipeCarbs] = useState();
-  //   const [fetchedRecipeCholesterol, setFetchedRecipeCholesterol] = useState();
-  //   const [fetchedRecipeFat, setFetchedRecipeFat] = useState();
+  const [fetchedRecipe, setFetchedRecipe] = useState();
 
   useEffect(() => {
     axios
       .get(recipeByNutrient)
       .then((response) => response.data)
-      .then((data) =>
-        //   setFetchedRecipeId(data[0].id),
-        //   setFetchedRecipeTitle(data[0].title),
-        //   setFetchedRecipeImage(data[0].image),
-        //   setFetchedRecipeCalories(data[0].calories),
-        //   setFetchedRecipeProtein(data[0].protein),
-        //   setFetchedRecipeCarbs(data[0].carbs),
-        //   setFetchedRecipeCholesterol(data[0].cholesterol),
-        //   setFetchedRecipeFat(data[0].saturatedFat)
-        data.filter(
-          (elt) => parseInt(elt.calories.slice(0 - 1) > 600) && parseInt(elt.fat.slice(0 - 1) > 50 && parseInt(elt.carbs.slice(0 - 1) > 50)),
-        ),
-      );
+      .then((data) => setFetchedRecipe(data.filter((elt) => elt.calories > 650 || parseInt(elt.fat.slice(0, -1)) > 60)));
   }, []);
 
   return (
     <div>
-      {/* <p>{fetchedRecipeId}</p>
-      <p>{fetchedRecipeTitle}</p>
-      <img src={fetchedRecipeImage} alt={fetchedRecipeTitle} />
-      <p>{fetchedRecipeCalories}</p>
-      <p>{fetchedRecipeProtein}</p>
-      <p>{fetchedRecipeCarbs}</p>
-      <p>{fetchedRecipeCholesterol}</p>
-      <p>{fetchedRecipeFat}</p> */}
+      <div>{fetchedRecipe && fetchedRecipe.slice(0, 10).map((info, index) => <RecipeCard key={index} {...info} />)}</div>
     </div>
   );
 }
