edd0558e72d3c7c6f35f53d13ebd2512db11c9c8
Your Name <you@example.com>
2021-11-04 13:04:38 +0100

    Add new componet Slider from Swiper library

---
+++ src/Components/Slider.jsx
@@ -0,0 +1,64 @@
+/*import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import Card from './Card';
+import './Slider.css';
+
+// import Swiper core and required modules
+import SwiperCore, { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Mousewheel, Keyboard]);
+*/
+
+import React from 'react';
+import Card from './Card';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './Slider.css';
+
+// import Swiper core and required modules
+import SwiperCore, { Pagination, Navigation } from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Pagination, Navigation]);
+
+export default function App() {
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Card />
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
