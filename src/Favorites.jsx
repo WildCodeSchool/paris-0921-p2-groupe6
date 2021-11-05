1e406395846a060c86df353f9119ecbd44eb4601
Eti-Fromentin <fromentin.etienne@gmail.com>
2021-11-05 11:10:33 +0100

    First Slider approved

--- src/Favorites.jsx
+++ src/Favorites.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import Slider from 'swiper';
-// import FetchRecipe from './Components/fetchrecipe';
-// import NutritionLabel from './Components/nutrition-label';
+
+import NutritionLabel from './Components/nutrition-label';
 
 function Favorites() {
   return (
     <main>
       <h1>Favoris</h1>
+      <NutritionLabel />
     </main>
   );
 }
