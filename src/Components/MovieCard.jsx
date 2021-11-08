6053c0c04fd9795661206cf4e34649a355ba9465
Your Name <you@example.com>
2021-11-08 16:11:56 +0100

    add movie card first try

---
+++ src/Components/MovieCard.jsx
@@ -0,0 +1,9 @@
+/*import React, { useState } from "react";
+import More from '../Assets/More.png';
+import './MovieCars.css';
+
+
+
+
+export default MovieCard;
+*/
---
+++ src/Components/fetchMovies.jsx
@@ -0,0 +1,9 @@
+//import axios from 'axios';
+import React from 'react';
+//import MovieCard from './MovieCard';
+
+function fetchMovies() {
+  return <div></div>;
+}
+
+export default fetchMovies;
