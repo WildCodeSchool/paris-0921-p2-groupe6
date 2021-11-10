d5e84200b07d01422ddced2ed6668765be80ec11
Your Name <you@example.com>
2021-11-10 17:24:12 +0100

    Add film slider

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import FetchMovies from './Components/fetchMovies';
 
 function AboutUs() {
   return (
     <main>
       <h1>A Propos</h1>
+      <FetchMovies />
     </main>
   );
 }
--- src/Components/fetchMovies.jsx
+++ src/Components/fetchMovies.jsx
@@ -1,9 +1,52 @@
-//import axios from 'axios';
-import React from 'react';
-//import MovieCard from './MovieCard';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import MovieCard from './MovieCard';
 
-function fetchMovies() {
-  return <div></div>;
+import 'swiper/css';
+import './Slider.css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import SwiperCore, { Pagination, Navigation } from 'swiper';
+
+SwiperCore.use([Pagination, Navigation]);
+
+function FetchMovies() {
+  const moviesCat =
+    'https://api.themoviedb.org/3/discover/movie?api_key=d174ca19b8b8536a5dcd5988d5132531&with_genres=35&with_genres=28&vote_average.gte=7&vote_average.lte=10';
+
+  const [fetchedMovies, setFetchedMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(moviesCat)
+      .then((response) => response.data)
+      .then((data) => setFetchedMovies(data.results));
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          className="mySwiper"
+        >
+          {fetchedMovies.length &&
+            fetchedMovies.slice(0, 10).map((movie, index) => (
+              <SwiperSlide key={index}>
+                <MovieCard key={index} title={movie.title} desc={movie.overview} img={movie.poster_path} />
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </div>
+    </div>
+  );
 }
 
-export default fetchMovies;
+export default FetchMovies;
