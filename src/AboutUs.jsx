324f4d6b5522fa83bb9a0d24ff139e775c023d41
sophielepert <sophie.lepert@gmail.com>
2021-11-12 15:34:46 +0100

    add the soft drinks part

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import FetchAlcoholDrink from './Components/fetchAlcoholDrink.jsx';
+import FetchSoftDrink from './Components/fetchSoftDrink.jsx';
 
 function AboutUs() {
   return (
     <main>
       <h1>A Propos</h1>
       <FetchAlcoholDrink />
+      <FetchSoftDrink />
     </main>
   );
 }
