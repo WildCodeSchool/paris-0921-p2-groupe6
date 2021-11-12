42df7b660a957595e27eef2f521ce8ce159dedc2
sophielepert <sophie.lepert@gmail.com>
2021-11-12 14:58:03 +0100

    change the color of some button to improve visibility

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import FetchDrink from './Components/fetchDrink.jsx';
 
 function AboutUs() {
   return (
     <main>
       <h1>A Propos</h1>
+      <FetchDrink />
     </main>
   );
 }
---
+++ src/Components/fetchDrink.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import axios from 'axios';
+
+import AlcoholDrinkCard from './AlcoholDrinkCard.jsx';
+
+import 'swiper/css';
+import './Slider.css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import SwiperCore, { Pagination, Navigation } from 'swiper';
+
+SwiperCore.use([Pagination, Navigation]);
+
+function FetchDrink() {
+  const drinksByCalories = 'http://localhost:8000/api/alcohol_drinks/';
+
+  const [fetchedDrink, setFetchedDrink] = useState();
+
+  useEffect(() => {
+    axios
+      .get(drinksByCalories)
+      .then((response) => response.data)
+      .then((data) => setFetchedDrink(data));
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          className="mySwiper"
+        >
+          {fetchedDrink &&
+            fetchedDrink.slice(0, 10).map((info, index) => (
+              <SwiperSlide key={index}>
+                <AlcoholDrinkCard key={index} {...info} />
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+}
+
+export default FetchDrink;
