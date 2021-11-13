c0dab897259b29393b026805676e8a2801399ded
Your Name <you@example.com>
2021-11-13 19:57:03 +0100

    Cancelled call of FetchMovie

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import FetchMovies from './Components/fetchMovies';
 
 function AboutUs() {
   return (
     <main>
       <h1>A Propos</h1>
-      <FetchMovies />
     </main>
   );
 }
