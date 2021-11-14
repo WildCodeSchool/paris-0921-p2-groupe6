363b8414632a25f81065e670efd95231fd4d05a4
sophielepert <sophie.lepert@gmail.com>
2021-11-14 13:27:56 +0100

    update aboutUs

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import FetchMovies from './Components/fetchMovies.jsx';
+import FetchAlcoholDrinks from './Components/fetchAlcoholDrink';
 
 function AboutUs() {
-  return <main></main>;
+  return (
+    <main>
+      <FetchAlcoholDrinks />
+      <FetchMovies />
+    </main>
+  );
 }
 
 export default AboutUs;
