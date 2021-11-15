a998da8cd53152df26af33da0416f307ce1e839a
Eti-Fromentin <fromentin.etienne@gmail.com>
2021-11-15 09:35:49 +0100

    canceled call api to get recipe, to redo

--- src/Components/RecipeCard.jsx
+++ src/Components/RecipeCard.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
+// import axios from 'axios';
 
 import More from '../Assets/More.png';
 
 import './RecipeCard.css';
 
-function RecipeCard({ id, image, title, calories, carbs, fat, protein, sugar }) {
+function RecipeCard({ image, title, calories, carbs, fat, protein, sugar }) {
   const [isFavorite, setIsFavorite] = useState(false);
   const [more, setMore] = useState(true);
   const [isAdded, setIsAdded] = useState(false);
-  const [moreInfo, setMoreInfo] = useState();
+  // const [moreInfo, setMoreInfo] = useState();
 
-  const getMoreInfoUrl = `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false&apiKey=7706683273f24fdcaf86cbbb8929f962`;
+  // const getMoreInfoUrl = `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false&apiKey=7706683273f24fdcaf86cbbb8929f962`;
 
   function handleClickFavorite() {
     setIsFavorite(!isFavorite);
@@ -25,12 +25,12 @@ function RecipeCard({ id, image, title, calories, carbs, fat, protein, sugar }) 
     setMore(!more);
   }
 
-  useEffect(() => {
-    axios
-      .get(getMoreInfoUrl)
-      .then((response) => response.data)
-      .then((data) => setMoreInfo(data));
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get(getMoreInfoUrl)
+  //     .then((response) => response.data)
+  //     .then((data) => setMoreInfo(data));
+  // }, []);
 
   return (
     <div className="recipe-Card">
@@ -60,7 +60,7 @@ function RecipeCard({ id, image, title, calories, carbs, fat, protein, sugar }) 
                 <div className="recipe-desc-details-li">Protein: {protein}</div>
                 <div className="recipe-desc-details-li">Sugar: {sugar}</div>
               </div>
-              {moreInfo.sourceUrl && (
+              {/* {moreInfo.sourceUrl && (
                 <a href={moreInfo.sourceUrl} target="_blank" rel="noreferrer">
                   Link
                 </a>
@@ -69,7 +69,7 @@ function RecipeCard({ id, image, title, calories, carbs, fat, protein, sugar }) 
                 <a href={moreInfo.spoonacularSourceUrl} target="_blank" rel="noreferrer">
                   Link 2
                 </a>
-              )}
+              )} */}
             </div>
             <button className="material-icons-outlined" id="close" onClick={handleClickFlip}>
               cancel
