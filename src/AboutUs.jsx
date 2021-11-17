1409b0e3627f20a57e1c099c965d79b856324b16
sophielepert <sophie.lepert@gmail.com>
2021-11-17 21:02:55 +0100

    create the aboutus page with desktop and mobile version and functionnal emailing

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+
+import Etienne from './Assets/Etienne.png';
 import Sophie from './Assets/sophieFood.png';
+import Vincenzo from './Assets/Vincenzo.png';
 import SmallLogo from './SmallLogo.png';
 
 import { NavLink } from 'react-router-dom';
+import { ContactUs } from './Components/ContactUs';
 
 import './AboutUs.css';
 
@@ -10,62 +14,44 @@ function AboutUs() {
   return (
     <main>
       <NavLink className="link" activeClassName="active" exact to="/">
-        <img src={SmallLogo} alt="Lazy Night Small Logo" className="FinalResultsLogo" />
+        <img src={SmallLogo} alt="Lazy Night Small Logo" className="AboutUsLogo" />
       </NavLink>
-      <h1>About Us</h1>
+      <h1 className="AboutUsTopFirstTitle">About Us</h1>
       <div className="AboutUsTop">
-        <h2 className="AboutUsTitle">We are your Fat Generator ! </h2>
+        <h2 className="AboutUsTitle">We are your Fat Generator! </h2>
         <p className="AboutUsConcept">
-          <em>Facts :</em> <br /> Fat act directly on your dopamines neurons that brings you pleasure and satisfaction. According to this research :
+          <em>Facts:</em> <br /> Fat act directly on your dopamines neurons that brings you pleasure and satisfaction. According to this research:
           Circulating Triglycerides Gate Dopamine- Associated Behaviors through DRD2-Expressing, there is a direct link between eating fat and
           pleasure.
         </p>
       </div>
       <div className="AboutTeam">
-        <h2 className="AboutUsTeamTitle">Who we are ?</h2>
-        <p className="AboutUsTeamPresentation">Food lovers, fat providers !</p>
+        <h2 className="AboutUsTeamTitle">Who are we?</h2>
+
         <div className="AboutUsMembers">
           <div className="AboutUsTeam">
             <h3 className="AboutUsTeamTitle">Etienne</h3>
-            <img src={Sophie} alt="Sophie eating poulp" className="portraitTeam" />
-            <p className="AboutUsTeam">likes chocolat, cheese and sometimes fight iwith her food !</p>
+            <img src={Etienne} alt="portrait of Etienne" className="portraitTeam" />
           </div>
           <div className="AboutUsTeam">
             <h3 className="AboutUsTeamTitle">Sophie </h3>
             <img src={Sophie} alt="Sophie eating poulp" className="portraitTeam" />
-            <p className="AboutUsTeam">likes chocolat, cheese and sometimes fight iwith her food !</p>
           </div>
           <div className="AboutUsTeam">
             <h3 className="AboutUsTeamTitle">Vincenzo</h3>
-            <img src={Sophie} alt="Sophie eating poulp" className="portraitTeam" />
-            <p className="AboutUsTeam">likes chocolat, cheese and sometimes fight iwith her food !</p>
+            <img src={Vincenzo} alt="Portrait of Vincenzo" className="portraitTeam" />
           </div>
         </div>
+        <p className="AboutUsTeamPresentation">
+          Food lovers, <br />
+          fat providers!
+        </p>
       </div>
       <div className="ContactUs">
         <h2 className="ContactUsTitle">Contact us</h2>
-        <p className="ContactUsText">
-          Any questions, requests, wishes or <br />
-          just to say you love our concept, feel free to contact us !
-        </p>
+        <p className="ContactUsText">Any questions, requests, wishes or just to say you love our concept, feel free to contact us!</p>
 
-        <form className="ContactForm">
-          <div className="FormGroupName">
-            <label htmlFor="name">Name</label>
-            <input type="text" className="form-control" />
-          </div>
-          <div className="FormGroupEmail">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" aria-describedby="emailHelp" />
-          </div>
-          <div className="FormGroupMessage">
-            <label htmlFor="message">Message</label>
-            <textarea className="form-control" rows="5"></textarea>
-          </div>
-          <button type="submit" className="FormBtn">
-            Submit
-          </button>
-        </form>
+        <ContactUs />
       </div>
     </main>
   );
