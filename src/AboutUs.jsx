89b502c9a4dd22ef0478edb9d304f76e160d056c
sophielepert <sophie.lepert@gmail.com>
2021-11-17 09:17:06 +0100

    content and style added on AboutUs

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -19,20 +19,47 @@ function AboutUs() {
         <h2 className="AboutUsTeamTitle">Who we are ?</h2>
         <p className="AboutUsTeamPresentation">Food lovers, fat providers !</p>
         <div className="AboutUsMembers">
-          <div className="AboutUsTeamEtienne">
+          <div className="AboutUsTeam">
             <h3 className="AboutUsTeamTitle">Etienne</h3>
-            <p className="AboutUsTeam"></p>
-          </div>
-          <div className="AboutUsTeamSophie">
-            <h3 className="AboutUsTeamTitle">Sophie </h3>
+            <img src={Sophie} alt="Sophie eating poulp" className="portraitTeam" />
             <p className="AboutUsTeam">likes chocolat, cheese and sometimes fight iwith her food !</p>
           </div>
-          <img src={Sophie} alt="Sophie eating poulp" className="portraitSophie" />
-          <div className="AboutUsTeamVincenzo">
+          <div className="AboutUsTeam">
+            <h3 className="AboutUsTeamTitle">Sophie </h3>
+            <img src={Sophie} alt="Sophie eating poulp" className="portraitTeam" />
+            <p className="AboutUsTeam">likes chocolat, cheese and sometimes fight iwith her food !</p>
+          </div>
+          <div className="AboutUsTeam">
             <h3 className="AboutUsTeamTitle">Vincenzo</h3>
-            <p className="AboutUsTeam"></p>
+            <img src={Sophie} alt="Sophie eating poulp" className="portraitTeam" />
+            <p className="AboutUsTeam">likes chocolat, cheese and sometimes fight iwith her food !</p>
           </div>
         </div>
+      </div>
+      <div className="ContactUs">
+        <h2 className="ContactUsTitle">Contact us</h2>
+        <p className="ContactUsText">
+          Any questions, requests, wishes or <br />
+          just to say you love our concept, feel free to contact us !
+        </p>
+
+        <form className="ContactForm">
+          <div className="FormGroupName">
+            <label htmlFor="name">Name</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="FormGroupEmail">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" aria-describedby="emailHelp" />
+          </div>
+          <div className="FormGroupMessage">
+            <label htmlFor="message">Message</label>
+            <textarea className="form-control" rows="5"></textarea>
+          </div>
+          <button type="submit" className="FormBtn">
+            Submit
+          </button>
+        </form>
       </div>
     </main>
   );
