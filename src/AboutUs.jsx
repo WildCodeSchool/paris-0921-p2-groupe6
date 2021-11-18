b49df9d154ad104469a8591624b1ad7ebec49761
sophielepert <sophie.lepert@gmail.com>
2021-11-18 17:36:31 +0100

    about us fix with a scroll to access the send button

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -15,42 +15,44 @@ function AboutUs() {
       <NavLink className="link" activeClassName="active" exact to="/">
         <img src={SmallLogo} alt="Lazy Night Small Logo" className="AboutUsLogo" />
       </NavLink>
-      <h1 className="AboutUsTopFirstTitle">About Us</h1>
-      <div className="AboutUsTop">
-        <h2 className="AboutUsTitle">We are your Fat Generator! </h2>
-        <p className="AboutUsConcept">
-          <em>Facts:</em> <br /> Fat act directly on your dopamines neurons that brings you pleasure and satisfaction. According to this research:
-          Circulating Triglycerides Gate Dopamine- Associated Behaviors through DRD2-Expressing, there is a direct link between eating fat and
-          pleasure.
-        </p>
-      </div>
-      <div className="AboutTeam">
-        <h2 className="AboutUsTeamTitle">Who are we?</h2>
-
-        <div className="AboutUsMembers">
-          <div className="AboutUsTeam">
-            <h3 className="AboutUsTeamTitle">Etienne</h3>
-            <img src={Etienne} alt="portrait of Etienne" className="portraitTeam" />
-          </div>
-          <div className="AboutUsTeam">
-            <h3 className="AboutUsTeamTitle">Sophie </h3>
-            <img src={Sophie} alt="Sophie eating poulp" className="portraitTeam" />
-          </div>
-          <div className="AboutUsTeam">
-            <h3 className="AboutUsTeamTitle">Vincenzo</h3>
-            <img src={Vincenzo} alt="Portrait of Vincenzo" className="portraitTeam" />
-          </div>
+      <div className="AboutUsBox">
+        <h1 className="AboutUsTopFirstTitle">About Us</h1>
+        <div className="AboutUsTop">
+          <h2 className="AboutUsTitle">We are your Fat Generator! </h2>
+          <p className="AboutUsConcept">
+            <em>Facts:</em> <br /> Fat act directly on your dopamines neurons that brings you pleasure and satisfaction. According to this research:
+            Circulating Triglycerides Gate Dopamine- Associated Behaviors through DRD2-Expressing, there is a direct link between eating fat and
+            pleasure.
+          </p>
         </div>
-        <p className="AboutUsTeamPresentation">
-          Food lovers, <br />
-          fat providers!
-        </p>
-      </div>
-      <div className="ContactUs">
-        <h2 className="ContactUsTitle">Contact us</h2>
-        <p className="ContactUsText">Any questions, requests, wishes or just to say you love our concept, feel free to contact us!</p>
+        <div className="AboutTeam">
+          <h2 className="AboutUsTeamTitle">Who are we?</h2>
 
-        <ContactUs />
+          <div className="AboutUsMembers">
+            <div className="AboutUsTeam">
+              <h3 className="AboutUsTeamTitle">Etienne</h3>
+              <img src={Etienne} alt="portrait of Etienne" className="portraitTeam" />
+            </div>
+            <div className="AboutUsTeam">
+              <h3 className="AboutUsTeamTitle">Sophie </h3>
+              <img src={Sophie} alt="Sophie eating poulp" className="portraitTeam" />
+            </div>
+            <div className="AboutUsTeam">
+              <h3 className="AboutUsTeamTitle">Vincenzo</h3>
+              <img src={Vincenzo} alt="Portrait of Vincenzo" className="portraitTeam" />
+            </div>
+          </div>
+          <p className="AboutUsTeamPresentation">
+            Food lovers, <br />
+            fat providers!
+          </p>
+        </div>
+        <div className="ContactUs">
+          <h2 className="ContactUsTitle">Contact us</h2>
+          <p className="ContactUsText">Any questions, requests, wishes or just to say you love our concept, feel free to contact us!</p>
+
+          <ContactUs />
+        </div>
       </div>
     </main>
   );
