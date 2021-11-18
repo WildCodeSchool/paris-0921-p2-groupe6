7e781cae262a42f4a6451b648028783d47baf676
Eti-Fromentin <fromentin.etienne@gmail.com>
2021-11-18 16:29:47 +0100

    e

--- src/Components/nutrition-label.jsx
+++ src/Components/nutrition-label.jsx
@@ -24,11 +24,7 @@ function NutritionLabel() {
               </th>
               <td>TOTAL CALORIE</td>
             </tr>
-            <tr className="thick-row">
-              {/* <td colSpan="3" className="small-info">
-                <b>% Daily Value*</b>
-              </td> */}
-            </tr>
+            <tr className="thick-row"></tr>
             <tr>
               <th colSpan="2">
                 <b>Total Fat</b>
@@ -44,29 +40,7 @@ function NutritionLabel() {
                 <b>TOTAL CARBS</b>
               </td>
             </tr>
-            {/* <tr>
-              <td className="blank-cell"></td>
-              <th>Trans Fat 0g</th>
-              <td></td>
-            </tr> */}
-            {/* <tr>
-              <th colSpan="2">
-                <b>Cholesterol</b>
-               
-              </th>
-              <td>
-                <b>TOTA</b>
-              </td>
-            </tr> */}
-            <tr>
-              {/* <th colSpan="2">
-                <b>Sodium</b>
-                40mg
-              </th>
-              <td>
-                <b>2%</b>
-              </td> */}
-            </tr>
+
             <tr>
               <th colSpan="2">
                 <b>Total Carbohydrate</b>
@@ -77,15 +51,9 @@ function NutritionLabel() {
             </tr>
             <tr>
               <td className="blank-cell"></td>
-              {/* <th>Dietary Fiber 1g</th>
-              <td>
-                <b>4%</b>
-              </td> */}
-            </tr>
-            <tr>
-              {/* <td className="blank-cell"></td> */}
+
               <th>Sugars</th>
-              <td></td>
+              {/* <td></td> */}
               <td>TOTAL SUGAR</td>
             </tr>
             <tr className="thick-end">
@@ -97,75 +65,6 @@ function NutritionLabel() {
             </tr>
           </tbody>
         </table>
-
-        {/* <table className="performance-facts__table--grid">
-          <tbody>
-            <tr>
-              <td colSpan="2">Vitamin A 10%</td>
-              <td>Vitamin C 0%</td>
-            </tr>
-            <tr className="thin-end">
-              <td colSpan="2">Calcium 10%</td>
-              <td>Iron 6%</td>
-            </tr>
-          </tbody>
-        </table> */}
-
-        <p className="small-info">
-          * Percent Daily Values are based on a 2,000 calorie diet. Your daily values may be higher or lower depending on your calorie needs:
-        </p>
-
-        <table className="performance-facts__table--small small-info">
-          <thead>
-            <tr>
-              <td colSpan="2"></td>
-              <th>Calories:</th>
-              <th>2,000</th>
-              <th>2,500</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th colSpan="2">Total Fat</th>
-              <td>Less than</td>
-              <td>65g</td>
-              <td>80g</td>
-            </tr>
-            <tr>
-              <td className="blank-cell"></td>
-              <th>Saturated Fat</th>
-              <td>Less than</td>
-              <td>20g</td>
-              <td>25g</td>
-            </tr>
-            <tr>
-              <th colSpan="2">Cholesterol</th>
-              <td>Less than</td>
-              <td>300mg</td>
-              <td>300 mg</td>
-            </tr>
-            <tr>
-              <th colSpan="2">Sodium</th>
-              <td>Less than</td>
-              <td>2,400mg</td>
-              <td>2,400mg</td>
-            </tr>
-            <tr>
-              <th colSpan="3">Total Carbohydrate</th>
-              <td>300g</td>
-              <td>375g</td>
-            </tr>
-            <tr>
-              <td className="blank-cell"></td>
-              <th colSpan="2">Dietary Fiber</th>
-              <td>25g</td>
-              <td>30g</td>
-            </tr>
-          </tbody>
-        </table>
-
-        <p className="small-info">Calories per gram:</p>
-        <p className="small-info text-center">Fat 9 &bull; Carbohydrate 4 &bull; Protein 4</p>
       </section>
     </div>
   );
